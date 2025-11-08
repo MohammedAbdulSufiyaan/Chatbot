bf4dc351f5832264a2e775468bf14bc583434ca4
MohammedAbdulSufiyaan <masufiyaan.5@gmail.com>
2025-11-08 18:52:54 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="SufiyaanAI",
+    version ="0.1.0",
+    author="Sufiyaan",
+    author_email ="masufiyaan.5@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
